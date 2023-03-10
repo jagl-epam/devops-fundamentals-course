e6298516d11c2c4c9f54a1fbabcfda78084ee62c
Jesus Gonzalez <jesus_alberto_gonzalez_lopez@epam.com>
2023-03-10 17:48:07 +0100

    Improvement of the code

--- tasks/module_4/task_1/watchDisk.sh
+++ tasks/module_4/task_1/watchDisk.sh
@@ -4,13 +4,13 @@ declare -i defaultThreshold=70
 declare -i threshold=$1
 re='[^0-9]+$'
 
-if [[ $threshold =~ $re ]]  
+if [[ ($1 =~ $re) || -z "$1" ]]
   then
-    diskUsage=$(df | sed s/%//g | awk -v threshold=$threshold '{ if($5 > threshold) print $0 }')
-  else
     echo -e "\033[31;40mA number was not provided, default threshold will be used\033[0m"
-    diskUsage=$(df | sed s/%//g | awk -v threshold=$defaultThreshold -F "[[:space:]]+"  '{ if($5 > threshold) print $0 }')
+    threshold=defaultThreshold
 fi
+
+diskUsage=$(df | sed s/%//g | awk -v threshold=$threshold -F "[[:space:]]+"  '{ if($5 > threshold) print $0 }')
 
 for var in "${diskUsage[@]}"
   do
