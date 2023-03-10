65ec19803a0d7a84a16432678122ea4c8a0919de
Jesus Gonzalez <jesus_alberto_gonzalez_lopez@epam.com>
2023-03-10 17:13:07 +0100

    Watching for a given threshold and printing the df command

--- tasks/module_4/task_1/watchDisk.sh
+++ tasks/module_4/task_1/watchDisk.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-defaultThreshold=70
+declare -i defaultThreshold=70
+declare -i threshold=$1
+re='[^0-9]+$'
 
-diskUsage=$(df | sed s/%//g | awk -v threshold=$defaultThreshold '{ if($5 > threshold) print $0 }')
-echo $diskUsage
+if [[ $threshold =~ $re ]]  
+  then
+    diskUsage=$(df | sed s/%//g | awk -v threshold=$threshold '{ if($5 > threshold) print $0 }')
+  else
+    echo -e "\033[31;40mA number was not provided, default threshold will be used\033[0m"
+    diskUsage=$(df | sed s/%//g | awk -v threshold=$defaultThreshold -F "[[:space:]]+"  '{ if($5 > threshold) print $0 }')
+fi
 
-
-
+for var in "${diskUsage[@]}"
+  do
+    echo "$var\n"
+done
